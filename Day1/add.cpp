46011787a06f36c20f5a1a8d8b43de4dd651d817
kabirsingh04 <kabirsingh0405@gmail.com>
2021-09-01 21:39:19 +0530

    added

--- Day1/add.cpp
+++ Day1/add.cpp
@@ -6,7 +6,15 @@ int main()
     cout << "Enter two variables \n";
     cin >> a >> b;
     int sum = a + b;
-    cout << sum;
+    int diff = a - b;
+    int prod = a * b;
+    int div = a / b;
+    int rem = a % b;
+    cout << "Sum = " << sum << endl;
+    cout << "Difference = " << diff << endl;
+    cout << "Product = " << prod << endl;
+    cout << "quotient = " << div << endl;
+    cout << "remainder = " << rem << endl;
 
     return 0;
 }
