de496acbcc2ec1ef93e481de3af088e9e94dc475
kabirsingh04 <kabirsingh0405@gmail.com>
2021-09-02 21:57:56 +0530

    added

--- Day2/if.cpp
+++ Day2/if.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-
+    int age;
+    cout << "Enter your age : " << endl;
+    cin >> age;
+    if (age >= 18)
+    {
+        cout << "You can vote.";
+    }
+    else
+    {
+        cout << "You can't vote.";
+    }
     return 0;
 }
