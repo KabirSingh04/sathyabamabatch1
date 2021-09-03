4e889ae5dd8b6480f2723be0e983628377d22196
kabirsingh04 <kabirsingh0405@gmail.com>
2021-09-03 21:21:54 +0530

    added

---
+++ Day2/abc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "Enter two Numbers : ";
+    cin >> x >> y;
+    cout << "X = " << x << endl;
+    cout << "Y = " << y << endl;
+    return 0;
+}
